931d9b81ae2cccae57cf6be08f72310f3295ecda
colegoddard <31323123+colegoddard@users.noreply.github.com>
2017-09-26 12:49:59 -0700

    Lab

--- Problem1.java
+++ Problem1.java
@@ -12,8 +12,56 @@ public class Problem1 extends Robot
     }
     
     public void sortBeepers() {
-        
+        initialPosition();
+        int beepersInColumn = 0;
+        int smallestColumn = 0;
+        int i = 0;
+        for (i = 0; i < 9; i++) {
+            int[] beepersInColumnStorage = new int[9];
+            while (nextToABeeper()) {
+                pickBeeper();
+                beepersInColumn = beepersInColumn + 1;
+                move();
+            }
+            beepersInColumnStorage[i + 0] = beepersInColumn;
+            for (int value : beepersInColumnStorage){
+                System.out.print(value);
+            }
+            beepersInColumn = 0;
+            reposition();
+        }
     }
-   
+    
+    public void initialPosition() {
+        move();
+        turnLeft();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        turnLeft();
+        turnLeft();
+    }
+    
+    public void reposition() {
+        turnRight();
+        move();
+        turnRight();
+        while (frontIsClear()) {
+            move();
+        }
+        turnRight();
+        turnRight();
+    }
+    
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    
 }
 
--- SortingTaskDriver.java
+++ SortingTaskDriver.java
@@ -70,7 +70,7 @@ public class SortingTaskDriver implements Directions
         World.setBeeperColor(Color.black);
         World.setStreetColor(Color.gray);
         World.setNeutroniumColor(Color.black);
-        World.setDelay(2);  
+        World.setDelay(20);  
         World.setVisible(true);
     }
 }
